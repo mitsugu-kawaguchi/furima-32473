5313ac2f3aead44255605d71fe510ceb0fb06e1e
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-11 15:02:24 +0900

    バリデーション、ファクトリーデータ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-  validates :password, presence: true,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
-  validates :family_name, presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]/,message: 'Full-width characters'}
-  validates :first_name, presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]/,message: 'Full-width characters'}
-  validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'}
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    with_options format: {with:/\A[ぁ-んァ-ヶ一-龥々]/,message: 'Full-width characters'} do
+      validates :family_name
+      validates :first_name
+    end
+    with_options format: {with: /\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'} do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname             {"yama"}
     email { Faker::Internet.unique.free_email }
-    password = Faker::Internet.password(min_length: 6) 
-    password {password}
-    password_confirmation {password}
+    password                { 'test1234TEST' }
+    password_confirmation   { password }
     family_name          {"山田"}
     first_name           {"太郎"}
     family_name_kana     {"ヤマダ"}
