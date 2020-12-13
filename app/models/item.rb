09bf9d29c161f064c5be0548fff0ffc97157d89d
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-13 10:50:20 +0900

    単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/, message:'Price half eidth numbers' }
     validates :description
     validates :image
     with_options numericality: { other_than: 1 } do
