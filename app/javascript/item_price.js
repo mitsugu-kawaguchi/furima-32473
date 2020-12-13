947603911a0add487097510a6c9df3e1822d36aa
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-13 16:18:39 +0900

    販売手数料・利益計算結果の表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,10 +3,9 @@ window.addEventListener('load', function(){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
-  })
-  const priceAdd = document.getElementById("add-tax-price");
-  console.log(priceAdd);
-  const priceProfit = document.getElementById("profit");
-  console.log(priceProfit);
-})
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue / 10);
+    const priceProfit = document.getElementById("profit");
+    priceProfit.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+ });
+});
