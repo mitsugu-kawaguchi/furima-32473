4a3a22d41c5507365789129125c8c4d7f37b66fe
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-13 13:51:20 +0900

    要素の取得

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,9 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById("item-price");
+  console.log(priceInput);
+  const priceAdd = document.getElementById("add-tax-price");
+  console.log(priceAdd);
+  const priceProfit = document.getElementById("profit");
+  console.log(priceProfit);
+})
