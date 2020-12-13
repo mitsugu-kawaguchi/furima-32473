cc9eecc0a3ff79b8079fee205b5f4694fb72fdb9
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-13 13:54:25 +0900

    イベントを発火

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function(){
 
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+  })
   const priceAdd = document.getElementById("add-tax-price");
   console.log(priceAdd);
   const priceProfit = document.getElementById("profit");
