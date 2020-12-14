753e6a787c04717809450969f5c1e6324c2214e2
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-14 14:14:47 +0900

    テストコード修正＃3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,31 +28,31 @@ RSpec.describe Item, type: :model do
       end
 
       it 'category_idが空だと出品できない' do
-        @item.category_id = ''
+        @item.category_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
       it 'status_idが空だと出品できない' do
-        @item.status_id = ''
+        @item.status_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
 
       it 'shipping_cost_idが空だと出品できない' do
-        @item.shipping_cost_id = ''
+        @item.shipping_cost_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
       end
 
       it 'prefecture_idが空だと出品できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
       it 'shipping_days_idが空だと出品できない' do
-        @item.shipping_days_id = ''
+        @item.shipping_days_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping days can't be blank")
       end
@@ -64,13 +64,13 @@ RSpec.describe Item, type: :model do
       end
 
       it '値段が300より小さかった時に出品できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
       it '値段が9999999より大きかった時に出品できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
@@ -82,31 +82,31 @@ RSpec.describe Item, type: :model do
       end
 
       it 'category_idの値が1であれば出品できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
 
       it 'status_idの値が1であれば出品できない' do
-        @item.status_id = '1'
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
 
       it 'shipping_cost_idの値が1であれば出品できない' do
-        @item.shipping_cost_id = '1'
+        @item.shipping_cost_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping cost must be other than 1')
       end
 
       it 'prefecture_idの値が1であれば出品できない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
 
       it 'shipping_days_idの値が1であれば出品できない' do
-        @item.shipping_days_id = '1'
+        @item.shipping_days_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping days must be other than 1')
       end
