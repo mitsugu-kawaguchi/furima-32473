03ab02cea6d6d6e67303058e8fe20472e2de6873
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-19 18:18:28 +0900

    自動デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "3.14.1"
 
 set :application, "furima-32473"
-set :repo_url, "git@example.com:mitsugu-kawaguchi/furima-32473.git"
+set :repo_url, "git@github.com:mitsugu-kawaguchi/furima-32473.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '176.34.1.17', user: 'ec2-user', roles: %w{app db web}
